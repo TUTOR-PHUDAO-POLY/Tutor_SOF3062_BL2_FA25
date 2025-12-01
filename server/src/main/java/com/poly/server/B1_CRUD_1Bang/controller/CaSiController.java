33b32ea5fd804ec47e101eed2ef93cab47d0bfc7
HangNT169 <hangnt169@fpt.edu.vn>
2025-12-01 15:56:39 +0700

    Buổi 4: Spring & VueJS

--- server/src/main/java/com/poly/server/B1_CRUD_1Bang/controller/CaSiController.java
+++ server/src/main/java/com/poly/server/B1_CRUD_1Bang/controller/CaSiController.java
@@ -4,6 +4,7 @@ import com.poly.server.B1_CRUD_1Bang.model.request.CaSiRequest;
 import com.poly.server.B1_CRUD_1Bang.model.response.CaSiResponse;
 import com.poly.server.B1_CRUD_1Bang.service.CaSiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/ca-si/management")
+@CrossOrigin(origins = "*")
 // Restfull api => BE => JSON
 // FE: doc dc JSON va hien thi
 public class CaSiController {
